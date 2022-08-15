ba1e0204cba9ff7ba82f9ce7c9878735e35ab32f
Tom Dahlberg <tom.dahlberg@regin.se>
2022-08-15 09:09:02 +0200

    Some simple source.

---
+++ source/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void setupArray(int *pArray)
+{
+    for(int i = 0; i < 9; i++)
+    {
+        pArray[i] = i;
+    }
+}
+
+void printArray(int *pArray)
+{
+    for(int i = 0; i < 11; i++)
+    {
+        printf("Array[%d] = %d", i, pArray[i]);
+    }
+}
+
+void main(void)
+{
+    int myArray[10];
+
+    setupArray(myArray);
+    printArray(myArray);
+}
